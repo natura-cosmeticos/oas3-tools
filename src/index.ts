530faa4eed942c3dea5a174f0dbc2f9a5123b3dc
ltakata-nat <leonardotakata@natura.net>
2024-02-21 14:07:24 -0300

    rollback version

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 'use strict';
 import { ExpressAppConfig } from "./middleware/express.app.config";
-import { Oas3AppOptions } from "./middleware/oas3.options";
-import { OpenApiRequestHandler } from 'express-openapi-validator/dist/framework/types'
 
-export function expressAppConfig(
-  definitionPath: string,
-  appOptions: Oas3AppOptions,
-  customMiddlewares?: OpenApiRequestHandler[]
-): ExpressAppConfig {
-  return new ExpressAppConfig(definitionPath, appOptions, customMiddlewares);
+export function expressAppConfig(definitionPath: string, routingOptions): ExpressAppConfig {
+  return new ExpressAppConfig(definitionPath, routingOptions);
 }
